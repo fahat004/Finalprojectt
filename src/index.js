a889b742b3d8f5b782cf12eab9cba7a1ce3fc7de
fahat004 <farhatfathy04@gmail.com>
2022-04-18 13:39:46 +0700

    hw module 5 ses 3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './asset/style/index.scss';
 import 'react-toastify/dist/ReactToastify.css';
-import App from './App';
+import App from './App.tsx';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import store from './store';
---
+++ src/types/image.ts
@@ -0,0 +1,6 @@
+export interface Image {
+    height: number | null;
+    url: string;
+    width: number | null;
+  }
+  
